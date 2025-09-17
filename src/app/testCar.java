17c4476f3294259461e742b6e7bcb88b686987f0
Lumpfur <127517560+Lumpfur@users.noreply.github.com>
2025-09-18 00:25:17 +0300

    testCar.java

--- src/app/testCar.java
+++ src/app/testCar.java
@@ -9,7 +9,7 @@ class TestCar {
         Car car = new Car("Toyota", "A123BC77", "Black", 2023, "Dima", "QQ123456B", "idk");
         ElectricCar eCar = new ElectricCar("Honda", "A121AA33", "White", 2023, "Danya", "AB123456C", 35.5);
 
-        car.setOwnerName("Yarik");
+        car.setOwnerName("Aik");
         eCar.setOwnerName("Akim");
         car.setColor("Yellow");
         eCar.setYear(2019);
